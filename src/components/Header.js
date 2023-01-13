497def7f10ff3c04bc9edc195a8fc04a8d29f4a4
yareds <37346809+yareds@users.noreply.github.com>
2023-01-13 00:41:49 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1> Yared - Todos List</h1>
+      <br />
+      <hr />
+    </div>
+  );
+};
+
+export default Header;
